8cc34fce9ad451dda711d45c7db62568ecb34f4d
Paul Kinsvater <paul.kinsvater@jci.com>
2023-02-19 09:44:17 +0100

    add altair vis to app

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import altair as alt
 import streamlit as st
 from metricflow import MetricFlowClient
 
@@ -6,9 +7,12 @@ client = MetricFlowClient.from_config()
 
 @st.cache_data
 def load_data(granularity: str = 'month'):
-    return client.query(metrics=['order_revenue_usd'],
+    data = client.query(metrics=['order_revenue_usd'],
                         dimensions=['site_id__site_region', f'metric_time__{granularity}'],
                         order=[f'metric_time__{granularity}']).result_df
+
+    data.columns = ['Period', 'Region', 'Revenue']
+    return data
 
 
 st.markdown('# Query data using a semantic layer')
@@ -16,5 +20,8 @@ st.markdown('# Query data using a semantic layer')
 choice = st.radio('Granularity', ('Month', 'Quarter', 'Year'))
 
 df = load_data(granularity=choice.lower())
+
+chart = alt.Chart(df).mark_line().encode(x='Period', y='Revenue', color='Region')
+st.altair_chart(chart, use_container_width=True)
 
 st.write(df)
