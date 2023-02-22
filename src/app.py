86bb687ac2ca3a57cbcb53e6c5e428bbe724b92f
Paul Kinsvater <paul.kinsvater@jci.com>
2023-02-22 09:21:34 +0100

    add 2nd metric

--- src/app.py
+++ src/app.py
@@ -6,8 +6,8 @@ client = MetricFlowClient.from_config()
 
 
 @st.cache_data
-def load_data(granularity: str = 'month'):
-    data = client.query(metrics=['order_revenue_usd'],
+def load_data(metric: str, granularity: str = 'month'):
+    data = client.query(metrics=[metric],
                         dimensions=['site_id__site_region', f'metric_time__{granularity}'],
                         order=[f'metric_time__{granularity}']).result_df
 
@@ -17,9 +17,11 @@ def load_data(granularity: str = 'month'):
 
 st.markdown('# Query data using a semantic layer')
 
-choice = st.radio('Granularity', ('Month', 'Quarter', 'Year'))
+choice_metric = st.radio('Metric', ('Revenue (USD)', 'Percent Margin'))
+choice_metric = 'order_revenue_usd' if choice_metric == 'Revenue (USD)' else 'order_margin_percent'
+choice_granularity = st.radio('Granularity', ('Month', 'Quarter', 'Year'))
 
-df = load_data(granularity=choice.lower())
+df = load_data(metric=choice_metric, granularity=choice_granularity.lower())
 
 chart = alt.Chart(df).mark_line().encode(x='Period', y='Revenue', color='Region')
 st.altair_chart(chart, use_container_width=True)
